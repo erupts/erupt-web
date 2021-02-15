1afba74b95856a6008dee3f4e27d31e7c6111ce2
yuepeng <erupts@126.com>
2021-02-15 23:50:13 +0800

    图片查看组件支持缩略图的方式快速多图切换

--- src/app/build/erupt/components/view-type/view-type.component.ts
+++ src/app/build/erupt/components/view-type/view-type.component.ts
@@ -1,54 +1,65 @@
-import { AfterViewInit, Component, Input, OnInit } from "@angular/core";
-import { View } from "../../model/erupt-field.model";
-import { EditType, ViewType } from "../../model/erupt.enum";
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from "@angular/core";
+import {View} from "../../model/erupt-field.model";
+import {EditType, ViewType} from "../../model/erupt.enum";
 import {DataService} from "@shared/service/data.service";
+import {STComponent} from "@delon/abc";
+import {NzCarouselComponent} from "ng-zorro-antd";
 
 @Component({
-  selector: "erupt-view-type",
-  templateUrl: "./view-type.component.html",
-  styleUrls: ["./view-type.component.less"],
-  styles: []
+    selector: "erupt-view-type",
+    templateUrl: "./view-type.component.html",
+    styleUrls: ["./view-type.component.less"],
+    styles: []
 })
 export class ViewTypeComponent implements OnInit, AfterViewInit {
 
-  @Input() view: View;
+    @Input() view: View;
 
-  @Input() value: any;
+    @Input() value: any;
 
-  @Input() eruptName: string;
+    @Input() eruptName: string;
 
-  show: boolean = false;
+    show: boolean = false;
 
-  paths: string[] = [];
+    paths: string[] = [];
 
-  viewType = ViewType;
+    viewType = ViewType;
 
-  constructor() {
-  }
-
-  ngOnInit() {
-    if (this.value) {
-      if (this.view.eruptFieldModel.eruptFieldJson.edit.type === EditType.ATTACHMENT) {
-        const attachmentType = this.view.eruptFieldModel.eruptFieldJson.edit.attachmentType;
-        let _paths = (<string>this.value).split(attachmentType.fileSeparator);
-        for (let path of _paths) {
-          this.paths.push(DataService.previewAttachment(path));
-        }
-      } else {
-        this.paths.push(DataService.previewAttachment(this.value));
-      }
-      switch (this.view.viewType) {
-        case ViewType.ATTACHMENT_DIALOG:
-          this.value = [DataService.previewAttachment(this.value)];
-          break;
-      }
+    constructor() {
     }
-  }
 
-  ngAfterViewInit() {
-    setTimeout(() => {
-      this.show = true;
-    }, 200);
-  }
+    ngOnInit() {
+        if (this.value) {
+            if (this.view.eruptFieldModel.eruptFieldJson.edit.type === EditType.ATTACHMENT) {
+                const attachmentType = this.view.eruptFieldModel.eruptFieldJson.edit.attachmentType;
+                let _paths = (<string>this.value).split(attachmentType.fileSeparator);
+                for (let path of _paths) {
+                    this.paths.push(DataService.previewAttachment(path));
+                }
+            } else {
+                this.paths.push(DataService.previewAttachment(this.value));
+            }
+            switch (this.view.viewType) {
+                case ViewType.ATTACHMENT_DIALOG:
+                    this.value = [DataService.previewAttachment(this.value)];
+                    break;
+            }
+        }
+    }
+
+    ngAfterViewInit() {
+        setTimeout(() => {
+            this.show = true;
+        }, 200);
+    }
+
+    @ViewChild("carousel", {static: false}) carouselComponent: NzCarouselComponent;
+
+    currIndex: number = 0;
+
+    goToCarouselIndex(index) {
+        this.carouselComponent.goTo(index);
+        this.currIndex = index;
+    }
 
 }
