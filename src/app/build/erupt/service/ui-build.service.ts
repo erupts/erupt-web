ef854fbf208a2314fb9c5abfe82494448370e7b3
yuepeng <erupts@126.com>
2021-07-06 00:40:33 +0800

    test

--- src/app/build/erupt/service/ui-build.service.ts
+++ src/app/build/erupt/service/ui-build.service.ts
@@ -91,8 +91,6 @@ export class UiBuildService {
                         break;
                 }
             }
-
-
             obj.width = titleWidth;
             //展示类型
             switch (view.viewType) {
