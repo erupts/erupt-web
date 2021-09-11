8e27302d5a1178093cd86c50b48efd5bd4fdd65f
yuepeng <erupts@126.com>
2021-09-11 17:55:35 +0800

    增加初始化配置项接口自检功能

--- src/app/core/startup/startup.service.ts
+++ src/app/core/startup/startup.service.ts
@@ -52,6 +52,10 @@ export class StartupService {
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     EruptAppData.put(<EruptAppModel>JSON.parse(xhr.responseText));
                     resolve();
+                } else {
+                    setTimeout(() => {
+                        location.reload();
+                    }, 3000);
                 }
             };
         });
