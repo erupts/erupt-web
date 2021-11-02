11a1b9d1594a8e3e6a367cd7b4a40ea008565a9a
yuepeng <erupts@126.com>
2021-11-02 23:17:04 +0800

    增加日语和繁体中文的多语言支持

--- src/app/core/startup/startup.service.ts
+++ src/app/core/startup/startup.service.ts
@@ -109,6 +109,7 @@ export class StartupService {
                             langData[key] = extra[key];
                         }
                     }
+                    console.log(langData);
                     this.translate.setTranslation(this.i18n.defaultLang, langData);
                     this.translate.setDefaultLang(this.i18n.defaultLang);
                 },
