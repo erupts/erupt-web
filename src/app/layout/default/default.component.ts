3e28d7128c08639a641885ff0a6fb660650bb6a5
yuepeng <erupts@126.com>
2022-01-05 23:15:24 +0800

    修复自定义首页刷新后不跳转的bug

--- src/app/layout/default/default.component.ts
+++ src/app/layout/default/default.component.ts
@@ -158,6 +158,7 @@ export class LayoutDefaultComponent implements OnInit, AfterViewInit, OnDestroy 
         this.notify$ = this.settings.notify.subscribe(() => this.setClass());
         this.setClass();
         //fill menu
+        this.tokenService.get().indexPath && this.router.navigateByUrl(this.tokenService.get().indexPath).then();
         this.data.getMenu().subscribe(res => {
             // this.statusService.menus = res;
             function generateTree(menus, pid): Menu[] {
@@ -236,7 +237,6 @@ export class LayoutDefaultComponent implements OnInit, AfterViewInit, OnDestroy 
                     }, 800);
                 });
             }
-
         });
     }
 
