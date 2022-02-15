2a74aa714a8cddbd7c8733a6052fb93bd7de7857
yuepeng <erupts@126.com>
2022-02-15 23:57:05 +0800

    多选树组件支持上下级联合选择能力

---
+++ src/app/shared/util/zorro.util.ts
@@ -0,0 +1,30 @@
+import {NzTreeNode} from "ng-zorro-antd/core/tree/nz-tree-base-node";
+
+export function calcChecks(nodes: NzTreeNode[]): any[] {
+    let set = new Set();
+
+    function putParents(node: NzTreeNode) {
+        if (node.getParentNode()) {
+
+            set.add(node.getParentNode().key);
+            putParents(node.parentNode);
+        }
+    }
+
+    function putChildren(node: NzTreeNode) {
+        if (node.getChildren() && node.getChildren().length > 0) {
+            for (let child of node.getChildren()) {
+                putChildren(child);
+                set.add(child.key);
+            }
+        }
+    }
+
+    for (let node of nodes) {
+        set.add(node.key);
+        putParents(node);
+        putChildren(node);
+    }
+
+    return Array.from(set);
+}
