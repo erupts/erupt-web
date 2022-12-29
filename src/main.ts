338b5d71f1ad8ea668b270cfffbab4a3d571c3a3
yuepeng <erupts@126.com>
2022-12-29 22:54:37 +0800

    重绘loading动画

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,22 @@ import {preloaderFinished} from '@delon/theme';
 import {AppModule} from './app/app.module';
 import {environment} from '@env/environment';
 
-preloaderFinished();
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  defaultEncapsulation: ViewEncapsulation.Emulated,
-  preserveWhitespaces: false
-}).then(res => {
-  const win = window as any;
-  if (win && win.appBootstrap) {
-    win.appBootstrap();
-  }
-  return res;
-}).catch(err => console.error(err));
+setTimeout(() => {
+        preloaderFinished();
+        window['SW'].stop();
+        window['SW'] = null;
+        if (environment.production) {
+            enableProdMode();
+        }
+        platformBrowserDynamic().bootstrapModule(AppModule, {
+            defaultEncapsulation: ViewEncapsulation.Emulated,
+            preserveWhitespaces: false
+        }).then(res => {
+            const win = window as any;
+            if (win && win.appBootstrap) {
+                win.appBootstrap();
+            }
+            return res;
+        }).catch(err => console.error(err));
+    }, 500
+)
