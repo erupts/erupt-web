4789643d46fe5f80a677ff10b723009a1c799a4a
yuepeng <erupts@126.com>
2023-01-08 22:02:16 +0800

    移除无效代码，修复菜单无法滑动的bug

--- src/app/core/net/default.interceptor.ts
+++ src/app/core/net/default.interceptor.ts
@@ -25,25 +25,6 @@ import {NzNotificationService} from "ng-zorro-antd/notification";
 import {NzMessageService} from "ng-zorro-antd/message";
 import {I18NService} from "../i18n/i18n.service";
 
-
-const CODEMESSAGE: { [key: number]: string } = {
-    200: '服务器成功返回请求的数据。',
-    201: '新建或修改数据成功。',
-    202: '一个请求已经进入后台排队（异步任务）。',
-    204: '删除数据成功。',
-    400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-    401: '用户没有权限（令牌、用户名、密码错误）。',
-    403: '用户得到授权，但是访问是被禁止的。',
-    404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-    406: '请求的格式不可得。',
-    410: '请求的资源被永久删除，且不会再得到的。',
-    422: '当创建一个对象时，发生一个验证错误。',
-    500: '服务器发生错误，请检查服务器。',
-    502: '网关错误。',
-    503: '服务不可用，服务器暂时过载或维护。',
-    504: '网关超时。'
-};
-
 /**
  * 默认HTTP拦截器，其注册细节见 `app.module.ts`
  */
@@ -70,16 +51,8 @@ export class DefaultInterceptor implements HttpInterceptor {
         setTimeout(() => this.injector.get(Router).navigateByUrl(url));
     }
 
-    private checkStatus(ev: HttpResponseBase): void {
-        if ((ev.status >= 200 && ev.status < 300) || ev.status === 401) {
-            return;
-        }
-        const errorText = CODEMESSAGE[ev.status] || ev.statusText;
-        this.notification.error(`请求错误 ${ev.status}: ${ev.url}`, errorText);
-    }
-
     private handleData(event: HttpResponse<any> | HttpErrorResponse): Observable<any> {
-        this.checkStatus(event)
+        // this.checkStatus(event)
         // 业务处理：一些通用操作
         switch (event.status) {
             case 200:
