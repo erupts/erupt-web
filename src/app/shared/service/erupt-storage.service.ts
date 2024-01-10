f137b35ae66208da062b968d2a6488e1b6649a37
yuepeng <erupts@126.com>
2024-01-10 21:44:40 +0800

    修复已选语言不回显的显示bug

--- src/app/shared/service/erupt-storage.service.ts
+++ src/app/shared/service/erupt-storage.service.ts
@@ -18,18 +18,45 @@ export class EruptStorageService {
         this.save(code, searchKey, query);
     }
 
-    saveColumnWidth(code: string, column: string, width: number) {
-        // this.save(code, columnKey, data);
+    getSearch(code: string): {
+        key: string,
+        value: any
+    }[] {
+        return this.get(code, searchKey);
     }
+
+    clearSearch(code: string) {
+        this.delete(code, searchKey);
+    }
+
+    // saveColumnWidth(code: string, column: string, width: number) {
+    //     let dataStr = localStorage.getItem(code);
+    //     let data = {};
+    //     if (!dataStr) {
+    //         data = JSON.parse(dataStr);
+    //     }
+    //     data["column"] = query;
+    //     localStorage.setItem(code, JSON.stringify(data));
+    // }
 
     private save(code: string, key: string, query: any) {
         let dataStr = localStorage.getItem(code);
         let data = {};
-        if (!dataStr) {
+        if (dataStr) {
             data = JSON.parse(dataStr);
         }
         data[key] = query;
+        console.log(code)
         localStorage.setItem(code, JSON.stringify(data));
+    }
+
+    private get(code: string, key: string): any {
+        let dataStr = localStorage.getItem(code);
+        if (dataStr) {
+            let data = JSON.parse(dataStr);
+            return data[key];
+        }
+        return null;
     }
 
     private delete(code: string, key: string) {
