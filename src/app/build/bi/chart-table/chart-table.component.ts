3f20d0fc02cf9c58ee5549e6bc8455d7f4fc7364
yuepeng <erupts@126.com>
2024-08-03 22:38:11 +0800

    修复bi数据表组件声明顺序与渲染顺序不一致的bug

--- src/app/build/bi/chart-table/chart-table.component.ts
+++ src/app/build/bi/chart-table/chart-table.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ChartApi} from "../model/bi.model";
 
 @Component({
     selector: 'erupt-chart-table',
@@ -11,7 +12,7 @@ export class ChartTableComponent implements OnInit, AfterViewInit {
     constructor() {
     }
 
-    data: any[];
+    chart: ChartApi
 
     // s2: TableSheet;
 
@@ -21,46 +22,11 @@ export class ChartTableComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit(): void {
-        // this.s2 = new TableSheet(this.chartTable.nativeElement, {
-        //     data: [],
-        //     fields: {},
-        // }, null);
-        // this.s2.render();
+
     }
 
-    render(data: any[]) {
-        this.data = data;
-        // let metas: Meta[] = [];
-        // let columns: string[] = [];
-        // if (data && data.length > 0) {
-        //     for (let key in data[0]) {
-        //         metas.push({field: key, name: key});
-        //         columns.push(key);
-        //     }
-        // }
-        // this.s2.setDataCfg({
-        //     data: data,
-        //     fields: {
-        //         columns: columns
-        //     },
-        //     meta: metas,
-        //     showDefaultHeaderActionIcon: true
-        // })
-        // this.onResize();
-        // this.s2.setThemeCfg({
-        //     // name: 'gray',
-        // });
-        // this.s2.render(true);
+    render(chart: ChartApi) {
+        this.chart = chart;
     }
-
-    // @HostListener('window:resize', ['$event'])
-    // onResize() {
-    //     if (this.s2) {
-    //         let ele = this.chartTable.nativeElement;
-    //         this.s2.changeSheetSize(ele.offsetWidth, ele.offsetHeight)
-    //         this.s2.render(false) // 不重新加载数据
-    //     }
-    // }
-
 
 }
