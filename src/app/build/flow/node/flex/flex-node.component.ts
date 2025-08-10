7d3edc1d895b600a7fa023ec31cda821bbf1cfd3
YuePeng <erupts@126.com>
2025-08-10 21:44:07 +0800

    optimize flex node

--- src/app/build/flow/node/flex/flex-node.component.ts
+++ src/app/build/flow/node/flex/flex-node.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NodeRule, NodeType} from "@flow/model/node.model";
-import {geneNodeId} from "@flow/util/flow-util";
+import {geneNodeId} from "@flow/util/flow.util";
 import {EruptBuildModel} from "../../../erupt/model/erupt-build.model";
 import {FlexNodeModel} from "@flow/model/flex-node.model";
 import {ANode} from "@flow/node/abstract-node";
