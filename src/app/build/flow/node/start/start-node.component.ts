f6aa5b20d8fa964d79c1db16e1633e1208fb7e5c
YuePeng <erupts@126.com>
2025-10-11 23:44:28 +0800

    optimize flow

--- src/app/build/flow/node/start/start-node.component.ts
+++ src/app/build/flow/node/start/start-node.component.ts
@@ -1,16 +1,19 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ANode} from "@flow/node/abstract-node";
 import {NodeRule, NodeType} from "@flow/model/node.model";
 import {geneNodeId, insertFlexNodeFun} from "@flow/util/flow.util";
 import {EruptBuildModel} from "../../../erupt/model/erupt-build.model";
 import {FlexNodeModel} from "@flow/model/flex-node.model";
+import {StartNode} from "@flow/model/fllw-approval.model";
+import {FormAccessEnum} from "@flow/model/flow.model";
 
 @Component({
     selector: 'app-start-node',
     templateUrl: './start-node.component.html',
     styleUrls: ['./start-node.component.less']
 })
-export class StartNodeComponent extends ANode {
+export class StartNodeComponent extends ANode implements OnInit {
+
     @Input() readonly = false;
     @Input() model: NodeRule;
     @Input() eruptBuild: EruptBuildModel;
@@ -20,6 +23,17 @@ export class StartNodeComponent extends ANode {
     @Output() select = new EventEmitter<any>();
     @Output() delete = new EventEmitter<any>();
     @Output() insertNode = new EventEmitter<any>();
+
+    startNode: StartNode = new StartNode();
+
+    ngOnInit(): void {
+        if (this.model.prop) {
+            this.startNode = this.model.prop;
+            if (!this.startNode.formAccesses) {
+                this.startNode.formAccesses = {};
+            }
+        }
+    }
 
     override onSelect() {
         this.select.emit(this.model);
@@ -62,6 +76,9 @@ export class StartNodeComponent extends ANode {
     }
 
     override onSaveProp(): void {
+        console.log(this.startNode)
+        this.model.prop = this.startNode;
     }
 
+    protected readonly FormAccessEnum = FormAccessEnum;
 }
