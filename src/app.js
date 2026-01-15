7fa8b69b188ae809ffd61c2f2405d86b373752bf
YuePeng <erupts@126.com>
2026-01-15 19:22:23 +0800

    Update theme and branding: change default logo text to "Erupt," set logoPath to null, update primary color, and reintroduce resource tools with external links.

--- src/app.js
+++ src/app.js
@@ -5,46 +5,46 @@ window.eruptSiteConfig = {
     desc: "Common Data Framework",
     dialogLogin: false,
     copyright: true, //是否保留显示版权信息
-    logoPath: "https://daas.automizely.org/logo.svg",
+    logoPath: null,
     logoFoldPath: null,
     loginLogoPath: null,
-    logoText: "DaaS",
+    logoText: "Erupt",
     registerPage: null,
     amapKey: '6ed167be6d22b8f8fa8e0402724df150',
     amapSecurityJsCode: "ee3e78fcb019e6078fcfc5b53b0a63ec",
     theme: {
-        primaryColor: '#ff6b2a'
+        primaryColor: '#1837ff'
     },
-    // r_tools: [{
-    //     mobileHidden: true,
-    //     icon: "fa-qq",
-    //     click: function (event) {
-    //         window.open("https://qm.qq.com/q/M3U23bwy6Q")
-    //     }
-    // }, {
-    //     mobileHidden: true,
-    //     icon: "fa-github",
-    //     click: function (event) {
-    //         window.open("https://github.com/erupts/erupt")
-    //     }
-    // }, {
-    //     mobileHidden: true,
-    //     icon: "fa-github-alt",
-    //     click: function (event) {
-    //         window.open("https://gitee.com/erupt/erupt")
-    //     }
-    // }, {
-    //     render: () => {
-    //         return `<div style="padding: 0 6px">
-    //                 <a class="align-self-center" href='https://gitee.com/erupt/erupt' target="_blank">
-    //                     <img alt='gitee star' src='https://gitee.com/erupt/erupt/badge/star.svg?theme=dark'/>
-    //                 </a>&nbsp;<a class="align-self-center" href='https://github.com/erupts/erupt' target="_blank">
-    //                     <img alt="github star" src="https://img.shields.io/github/stars/erupts/erupt?style=social">
-    //                 </a>
-    //             </div>`
-    //     },
-    //     mobileHidden: true
-    // }],
+    r_tools: [{
+        mobileHidden: true,
+        icon: "fa-qq",
+        click: function (event) {
+            window.open("https://qm.qq.com/q/M3U23bwy6Q")
+        }
+    }, {
+        mobileHidden: true,
+        icon: "fa-github",
+        click: function (event) {
+            window.open("https://github.com/erupts/erupt")
+        }
+    }, {
+        mobileHidden: true,
+        icon: "fa-github-alt",
+        click: function (event) {
+            window.open("https://gitee.com/erupt/erupt")
+        }
+    }, {
+        render: () => {
+            return `<div style="padding: 0 6px">
+                    <a class="align-self-center" href='https://gitee.com/erupt/erupt' target="_blank">
+                        <img alt='gitee star' src='https://gitee.com/erupt/erupt/badge/star.svg?theme=dark'/>
+                    </a>&nbsp;<a class="align-self-center" href='https://github.com/erupts/erupt' target="_blank">
+                        <img alt="github star" src="https://img.shields.io/github/stars/erupts/erupt?style=social">
+                    </a>
+                </div>`
+        },
+        mobileHidden: true
+    }],
     login: function (e) {
 
     },
