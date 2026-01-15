6d2c999c1c7e7128da5c44adb58a48f81f6ff798
YuePeng <erupts@126.com>
2026-01-15 19:21:40 +0800

    Refactor CKEditor component: replace DecoupledDocumentEditor with CKEditor 4, add lifecycle hooks for initialization and cleanup, and streamline editor integration logic.

--- src/app/build/erupt/components/ckeditor/ckeditor.component.ts
+++ src/app/build/erupt/components/ckeditor/ckeditor.component.ts
@@ -1,21 +1,11 @@
-import {
-    AfterViewInit,
-    Component,
-    ElementRef,
-    EventEmitter,
-    Inject,
-    Input,
-    OnDestroy,
-    OnInit,
-    Output
-} from "@angular/core";
+import {Component, ElementRef, EventEmitter, Inject, Input, OnInit, Output} from "@angular/core";
 import {LazyService} from "@delon/util";
 import {EruptFieldModel} from "../../model/erupt-field.model";
 import {EruptModel} from "../../model/erupt.model";
 import {RestPath} from "../../model/erupt.enum";
 import {DA_SERVICE_TOKEN, ITokenService} from "@delon/auth";
 
-declare const CKEDITOR: any;
+declare const DecoupledDocumentEditor;
 
 @Component({
     standalone: false,
@@ -23,7 +13,7 @@ declare const CKEDITOR: any;
     templateUrl: "./ckeditor.component.html",
     styles: []
 })
-export class CkeditorComponent implements AfterViewInit,OnDestroy {
+export class CkeditorComponent implements OnInit {
 
     @Input() eruptField: EruptFieldModel;
 
@@ -39,45 +29,117 @@ export class CkeditorComponent implements AfterViewInit,OnDestroy {
 
     editorError: boolean = false;
 
-    private editorInstance: any;
-
     constructor(private lazy: LazyService, private ref: ElementRef,
                 @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService) {
     }
 
-    ngAfterViewInit() {
+    ngOnInit() {
         let that = this;
-        this.lazy.loadScript("assets/ckeditor4/ckeditor.js").then(() => {
-            this.editorInstance = CKEDITOR.replace(this.ref.nativeElement.querySelector("#editor"), {
-                language: "zh-cn",
-                filebrowserUploadUrl: RestPath.file + "/upload-html-editor/" + this.erupt.eruptName + "/" +
-                    this.eruptField.fieldName + "?_erupt=" + this.erupt.eruptName + "&_token=" + this.tokenService.get().token,
-                readOnly: this.readonly
+        setTimeout(() => {
+            this.lazy.loadScript("assets/js/ckeditor.js").then(() => {
+                DecoupledDocumentEditor.create(this.ref.nativeElement.querySelector("#editor"), {
+                    toolbar: {
+                        items: [
+                            'heading',
+                            '|',
+                            'fontSize',
+                            'fontFamily',
+                            'fontBackgroundColor',
+                            'fontColor',
+                            '|',
+                            'bold',
+                            'italic',
+                            'underline',
+                            'strikethrough',
+                            '|',
+                            'alignment',
+                            '|',
+                            'numberedList',
+                            'bulletedList',
+                            '|',
+                            'indent',
+                            'outdent',
+                            '|',
+                            'link',
+                            'imageUpload',
+                            'insertTable',
+                            'codeBlock',
+                            'blockQuote',
+                            'highlight',
+                            '|',
+                            'undo',
+                            'redo',
+                            '|',
+                            'code',
+                            'horizontalLine',
+                            'subscript',
+                            'todoList',
+                            'mediaEmbed'
+                        ]
+                    },
+                    image: {
+                        toolbar: [
+                            'imageTextAlternative',
+                            'imageStyle:full',
+                            'imageStyle:side'
+                        ]
+                    },
+                    table: {
+                        contentToolbar: [
+                            'tableColumn',
+                            'tableRow',
+                            'mergeTableCells'
+                        ]
+                    },
+                    licenseKey: '',
+                    language: "zh-cn",
+                    ckfinder: {
+                        uploadUrl: RestPath.file + "/upload-html-editor/" + this.erupt.eruptName + "/" +
+                            this.eruptField.fieldName + "?_erupt=" + this.erupt.eruptName + "&_token=" + this.tokenService.get().token
+                    },
+                    // mediaEmbed: {
+                    //     providers: [
+                    //         {
+                    //             name: 'myprovider',
+                    //             url: [
+                    //                 /^lizzy.*\.com.*\/media\/(\w+)/,
+                    //                 /^www\.lizzy.*/,
+                    //                 /^.*/
+                    //             ],
+                    //             html: match => {
+                    //                 //获取媒体url
+                    //                 const input = match['input'];
+                    //                 //console.log('input' + match['input']);
+                    //                 return (
+                    //                     '<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 70%;">' +
+                    //                     `<iframe src="${input}" ` +
+                    //                     'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+                    //                     'frameborder="0" allowtransparency="true" allow="encrypted-media">' +
+                    //                     '</iframe>' +
+                    //                     '</div>'
+                    //                 );
+                    //             }
+                    //         }
+                    //     ]
+                    // }
+                }).then(editor => {
+                    editor.isReadOnly = this.readonly;
+                    that.loading = false;
+                    const toolbarContainer = this.ref.nativeElement.querySelector("#toolbar-container");
+                    toolbarContainer.appendChild(editor.ui.view.toolbar.element);
+                    if (that.value) {
+                        editor.setData(that.value);
+                    }
+                    editor.model.document.on("change:data", function () {
+                        that.valueChange.emit(editor.getData());
+                    });
+                }).catch(error => {
+                    this.loading = false;
+                    this.editorError = true;
+                    console.error(error);
+                });
             });
-
-            this.editorInstance.on('instanceReady', function () {
-                that.loading = false;
-                if (that.value) {
-                    this.editorInstance.setData(that.value);
-                }
-            });
-
-            this.editorInstance.on('change', function () {
-                that.valueChange.emit(this.editorInstance.getData());
-            });
-        }).catch(error => {
-            this.loading = false;
-            this.editorError = true;
-            console.error(error);
-        });
-    }
-
-    ngOnDestroy() {
-        console.log('CKEditor component destroyed');
-        if (this.editorInstance) {
-            this.editorInstance.destroy(true);
-            this.editorInstance = null;
-        }
+        }, 200);
     }
 
 }
