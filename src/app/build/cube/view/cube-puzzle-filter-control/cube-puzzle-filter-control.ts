5b22e1dc9bcc64abf0995888c85803f96ab266ac
YuePeng <erupts@126.com>
2026-01-29 20:53:18 +0800

    Optimize cube

--- src/app/build/cube/view/cube-puzzle-filter-control/cube-puzzle-filter-control.ts
+++ src/app/build/cube/view/cube-puzzle-filter-control/cube-puzzle-filter-control.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Dashboard, FilterControl, FilterDSL} from "../../model/dashboard.model";
-import {CubeMeta} from "../../model/cube.model";
+import {CubeMeta, FieldType} from "../../model/cube.model";
 import {CubeOperator} from "../../model/cube-query.model";
 import {CubeApiService} from "../../service/cube-api.service";
 
@@ -20,7 +20,7 @@ export class CubePuzzleFilterControl implements OnInit {
 
     @Input() size: "large" | "small" | "default" = "default";
 
-    data: Record<string, any>[] = [];
+    data: Record<string, any>[] = null;
 
     isLoading = false;
 
@@ -28,29 +28,54 @@ export class CubePuzzleFilterControl implements OnInit {
     }
 
     ngOnInit(): void {
-        this.reload();
     }
 
-    reload() {
-        this.isLoading = true;
-        if (this.filter.field) {
-            if (this.cubeMeta.parameters.filter(it => it.code == this.filter.field).length == 0) {
-                this.cubeApiService.query({
-                    cube: this.cubeMeta.code,
-                    explore: this.dashboard.explore,
-                    dimensions: [this.filter.field],
-                    measures: [],
-                    limit: 300,
-                }).subscribe(res => {
-                    this.data = res.data;
-                    this.isLoading = false;
-                })
-            } else {
-                //TODO fetch parameters list
+    fieldType(): FieldType {
+        for (let dimension of this.cubeMeta.dimensions) {
+            if (dimension.code === this.filter.field) {
+                return dimension.type;
+            }
+        }
+        for (let measure of this.cubeMeta.measures) {
+            if (measure.code === this.filter.field) {
+                return measure.type;
+            }
+        }
+        for (let parameter of this.cubeMeta.parameters) {
+            if (parameter.code === this.filter.field) {
+                return parameter.type;
+            }
+        }
+        return FieldType.STRING;
+    }
+
+    clean() {
+        this.data = null;
+    }
+
+    openSelect() {
+        if (!this.data) {
+            this.isLoading = true;
+            if (this.filter.field) {
+                if (this.cubeMeta.parameters.filter(it => it.code == this.filter.field).length == 0) {
+                    this.cubeApiService.query({
+                        cube: this.cubeMeta.code,
+                        explore: this.dashboard.explore,
+                        dimensions: [this.filter.field],
+                        measures: [],
+                        limit: 300,
+                    }).subscribe(res => {
+                        this.data = res.data;
+                        this.isLoading = false;
+                    })
+                } else {
+                    //TODO fetch parameters list
+                }
             }
         }
     }
 
     protected readonly CubeOperator = CubeOperator;
     protected readonly FilterControl = FilterControl;
+    protected readonly FieldType = FieldType;
 }
