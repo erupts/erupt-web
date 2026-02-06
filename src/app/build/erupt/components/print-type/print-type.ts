42e00376311ba9b3329aa2be9a90a246d2cdc020
YuePeng <erupts@126.com>
2026-02-06 20:39:19 +0800

    Erupt support print

--- src/app/build/erupt/components/print-type/print-type.ts
+++ src/app/build/erupt/components/print-type/print-type.ts
@@ -3,10 +3,8 @@ import {EruptFieldModel} from "../../model/erupt-field.model";
 import {AttachmentEnum, ChoiceEnum, DateEnum, EditType, FormSize, MultiChoiceEnum} from "../../model/erupt.enum";
 import {DataService} from "@shared/service/data.service";
 import {EruptModel} from "../../model/erupt.model";
-import {colRules} from "@shared/model/util.model";
 import {EruptBuildModel} from "../../model/erupt-build.model";
 import {I18NService} from "@core";
-import printJS from "print-js";
 
 @Component({
     standalone: false,
@@ -19,11 +17,10 @@ export class PrintTypeComponent implements OnInit {
     //important
     @Input() eruptBuildModel: EruptBuildModel;
 
-    //UI
-    @Input() col = colRules[3];
+    @Input() bordered = false;
 
     //UI
-    @Input() labelWidth: number = 120;
+    @Input() col = 3;
 
     eruptModel: EruptModel;
 
@@ -47,7 +44,7 @@ export class PrintTypeComponent implements OnInit {
         this.eruptModel = this.eruptBuildModel.eruptModel;
         let layout = this.eruptModel.eruptJson.layout;
         if (layout && layout.formSize == FormSize.FULL_LINE) {
-            this.col = colRules[1];
+            this.col = 1;
         }
         for (let model of this.eruptModel.eruptFieldModels) {
             switch (model.eruptFieldJson.edit.type) {
@@ -129,22 +126,7 @@ export class PrintTypeComponent implements OnInit {
     }
 
     print() {
-        printJS({
-            printable: this.elementRef.nativeElement.querySelector('.print-preview-container'),
-            type: 'html',
-            targetStyles: ['*'],
-            style: `
-                * {
-                    font-family: 'Heiti SC', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
-                    -webkit-print-color-adjust: exact !important;
-                    print-color-adjust: exact !important;
-                }
-
-                @page {
-                    size: A4 portrait;
-                    margin: 10mm 10mm 10mm 10mm;
-                }
-              `
-        });
+        // 使用原生打印，确保显示和打印完全一致
+        window.print();
     }
 }
