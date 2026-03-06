2ee83cf0cbb605b5e1e13c8af02653acfc2d1b1c
YuePeng <erupts@126.com>
2026-03-06 23:46:21 +0800

    lunch-flow-drawer support change width

--- src/app/build/flow/view/flow-dashboard/create-instance/create-instance.component.ts
+++ src/app/build/flow/view/flow-dashboard/create-instance/create-instance.component.ts
@@ -1,4 +1,5 @@
-import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Inject, Input, NgZone, OnDestroy, OnInit, Output} from '@angular/core';
+import {NzDrawerRef, NzDrawerService} from "ng-zorro-antd/drawer";
 import {FormSize} from "../../../../erupt/model/erupt.enum";
 import {FlowApiService} from "@flow/service/flow-api.service";
 import {EruptBuildModel} from "../../../../erupt/model/erupt-build.model";
@@ -6,9 +7,7 @@ import {FlowInstanceApiService} from "@flow/service/flow-instance-api.service";
 import {FlowConfig} from "@flow/model/flow.model";
 import {DataHandlerService} from "../../../../erupt/service/data-handler.service";
 import {NzMessageService} from "ng-zorro-antd/message";
-import {NzModalService} from "ng-zorro-antd/modal";
 import {EruptFlowComponent} from "@flow/components/erupt-flow/erupt-flow.component";
-import {NzDrawerService} from "ng-zorro-antd/drawer";
 import {StartNode} from "@flow/model/flow-approval.model";
 
 @Component({
@@ -17,7 +16,7 @@ import {StartNode} from "@flow/model/flow-approval.model";
     templateUrl: './create-instance.component.html',
     styleUrls: ['./create-instance.component.less']
 })
-export class CreateInstanceComponent implements OnInit {
+export class CreateInstanceComponent implements OnInit, OnDestroy {
 
     @Input() flow: FlowConfig;
 
@@ -33,16 +32,75 @@ export class CreateInstanceComponent implements OnInit {
 
     eruptBuild: EruptBuildModel;
 
+    private resizing = false;
+    private startX = 0;
+    private startWidth = 0;
+    private animationId: number | null = null;
+    private drawerElement: HTMLElement | null = null;
+
     constructor(private msg: NzMessageService,
-                @Inject(NzModalService)
-                private modal: NzModalService,
                 private dataHandlerService: DataHandlerService,
                 @Inject(NzDrawerService)
                 private drawerService: NzDrawerService,
+                private drawerRef: NzDrawerRef,
+                private el: ElementRef,
+                private ngZone: NgZone,
                 private flowApiService: FlowApiService,
                 private flowInstanceApiService: FlowInstanceApiService) {
 
     }
+
+    onMouseDown(event: MouseEvent): void {
+        this.resizing = true;
+        this.startX = event.clientX;
+        this.startWidth = parseInt(this.drawerRef.nzWidth as string) || 520;
+
+        // 查找抽屉的容器 DOM 元素
+        if (!this.drawerElement) {
+            let parent = this.el.nativeElement.parentElement;
+            while (parent && !parent.classList.contains('ant-drawer-content-wrapper')) {
+                parent = parent.parentElement;
+            }
+            this.drawerElement = parent;
+        }
+
+        this.ngZone.runOutsideAngular(() => {
+            document.addEventListener('mousemove', this.onMouseMove);
+            document.addEventListener('mouseup', this.onMouseUp);
+        });
+        event.preventDefault();
+    }
+
+    private onMouseMove = (event: MouseEvent) => {
+        if (!this.resizing) return;
+        if (this.animationId) {
+            cancelAnimationFrame(this.animationId);
+        }
+        this.animationId = requestAnimationFrame(() => {
+            const offset = this.startX - event.clientX;
+            const newWidth = Math.max(300, Math.min(window.innerWidth * 0.9, this.startWidth + offset));
+            if (this.drawerElement) {
+                this.drawerElement.style.width = `${newWidth}px`;
+            }
+        });
+    };
+
+    private onMouseUp = (event: MouseEvent) => {
+        this.resizing = false;
+        if (this.animationId) {
+            cancelAnimationFrame(this.animationId);
+            this.animationId = null;
+        }
+        document.removeEventListener('mousemove', this.onMouseMove);
+        document.removeEventListener('mouseup', this.onMouseUp);
+
+        // 同步状态到 NzDrawerRef，确保 Angular 知晓最终宽度
+        const offset = this.startX - event.clientX;
+        const finalWidth = Math.max(300, Math.min(window.innerWidth * 0.9, this.startWidth + offset));
+        this.ngZone.run(() => {
+            this.drawerRef.nzWidth = `${finalWidth}px`;
+        });
+    };
 
     ngOnInit() {
         this.loading = true;
@@ -59,6 +117,14 @@ export class CreateInstanceComponent implements OnInit {
             })
             this.startNode = this.flow.rule[0].prop;
         }
+    }
+
+    ngOnDestroy() {
+        if (this.animationId) {
+            cancelAnimationFrame(this.animationId);
+        }
+        document.removeEventListener('mousemove', this.onMouseMove);
+        document.removeEventListener('mouseup', this.onMouseUp);
     }
 
     onViewFlow() {
