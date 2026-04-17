1cae5bfaa14f92c62cead65b1da5f66be8c448c3
YuePeng <erupts@126.com>
2026-04-17 23:21:27 +0800

    Fix date convert bug

--- src/app/build/erupt/components/print-type/print-type.ts
+++ src/app/build/erupt/components/print-type/print-type.ts
@@ -80,7 +80,7 @@ export class PrintTypeComponent implements OnInit {
                         format = "yyyy-MM-dd";
                         break;
                     case DateEnum.DATE_TIME:
-                        format = "yyyy-MM-dd'T'HH:mm:ss.SSS";
+                        format = "yyyy-MM-dd HH:mm:ss";
                         break;
                     case DateEnum.MONTH:
                         format = "yyyy-MM";
